66b667a81db844a1ef2f4901eb05fe882d5d9ef7
Michelle Truong <mitruong@ucsd.edu>
2024-06-14 09:36:59 -0700

    Add shapefile and sample data. Leaflet map not loading.

--- app.R
+++ app.R
@@ -7,13 +7,16 @@
 #    https://shiny.posit.co/
 #
 
+library(here)
+library(tidyverse)
 library(shiny)
+library(leaflet)
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
 
     # Application title
-    titlePanel("Old Faithful Geyser Data"),
+    titlePanel("San Diego Demo Map"),
 
     # Sidebar with a slider input for number of bins 
     sidebarLayout(
@@ -25,26 +28,39 @@ ui <- fluidPage(
                         value = 30)
         ),
 
-        # Show a plot of the generated distribution
+        # Show map of San Diego zipcodes and testing sites
         mainPanel(
-           plotOutput("distPlot")
+           leafletOutput("mapTestingSites", height = 700)
         )
     )
 )
 
 # Define server logic required to draw a histogram
 server <- function(input, output, session) {
+  
+  # San Diego zipcode shapefile
+  sd_zip_shp <- sf::st_read(here("shapefile", "geo_sdcounty.shp"))
+  
+  # Get testing site data
+  testing_sites <- read.csv(here("data", "SanDiego_Testing_Sites_20240502.csv"))
+  
+  testing_map <- testing_sites |> 
+    leaflet() |> 
+    addProviderTiles("CartoDB.Positron") |> 
+    addPolygons(data = sd_zip_shp,
+                weight = 0.5,
+                color = "#333",
+                fillOpacity = 0.3,
+                smoothFactor = 0.5,
+                highlightOptions = highlightOptions(weight = 2.0,
+                                                    color = "#FFF"),
+                label = ~ zip) |> 
+    addMarkers(data = testing_sites, lng = ~ Longitude, lat = ~ Latitude, popup = ~ Name)
+  
 
-    output$distPlot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white',
-             xlab = 'Waiting time to next eruption (in mins)',
-             main = 'Histogram of waiting times')
-    })
+  output$mapTestingSites <- renderLeaflet({
+    testing_map
+  })
 }
 
 # Run the application 
